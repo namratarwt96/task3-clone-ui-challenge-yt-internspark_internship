dcb515a789b588f238f3a5b0517baec628620109
Namrata Rawat <namratarwt96@gmail.com>
2026-07-26 10:46:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 # Functionality of YOUTUBE UI clone
+
+
+const menu = document.querySelector(".menu");
+const sidebar = document.querySelector(".sidebar");
+
+menu.addEventListener("click", () => {
+    sidebar.classList.toggle("hide");
+});
+
+
+
+const buttons = document.querySelectorAll(".categories button");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        buttons.forEach(btn => {
+            btn.classList.remove("active");
+        });
+
+        button.classList.add("active");
+
+    });
+
+});
